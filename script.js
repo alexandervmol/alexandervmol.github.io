a6d4d7a05f41679598d9501246be74b03cdc042e
alexandervmol <mvalexander1012@gmail.com>
2026-07-19 12:11:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -249,22 +249,31 @@ gsap.utils.toArray(".art-piece img").forEach(image=>{
 // RECENT EXHIBITION SLIDER
 // =====================================================
 
-const swiper = new Swiper(".mySwiper",{
+const exhibitionSwiper = new Swiper(".exhibitionSwiper", {
 
-    loop:true,
+    loop: true,
 
-    speed:1800,
+    speed: 1800,
 
-    effect:"fade",
+    effect: "fade",
 
-    fadeEffect:{
-        crossFade:true
+    fadeEffect: {
+        crossFade: true
     },
 
-    autoplay:{
-        delay:4000,
-        disableOnInteraction:false
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
     }
 
 });
-
