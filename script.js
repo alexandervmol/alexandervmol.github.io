34f2def77bba5e95e5db327bbec505c234321e5a
alexandervmol <mvalexander1012@gmail.com>
2026-07-20 22:38:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -267,3 +267,14 @@ const swiper = new Swiper(".exhibitionSwiper",{
     }
 
 });
+
+
+const menuButton = document.querySelector(".menu-toggle");
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
+menuButton.addEventListener("click",()=>{
+
+    mobileMenu.classList.toggle("active");
+
+});
